74d4342d4514d9408789f7959f71b0c55ab65569
Jason Reilly <jdr0887@gmail.com>
2017-01-27 13:14:11 -0500

    removing binning home

--- diagnostic-gs-executor/src/main/java/org/renci/binning/diagnostic/gs/executor/DiagnosticGSTask.java
+++ diagnostic-gs-executor/src/main/java/org/renci/binning/diagnostic/gs/executor/DiagnosticGSTask.java
@@ -1,7 +1,5 @@
 package org.renci.binning.diagnostic.gs.executor;
 
-import static org.renci.binning.core.Constants.BINNING_HOME;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,11 +44,6 @@ public class DiagnosticGSTask implements Runnable {
 
             Map<String, Object> variables = new HashMap<String, Object>();
             variables.put("binningJobId", binningJobId);
-            variables.put("irods.home", "/projects/mapseq/apps/irods-4.2.0/icommands");
-            // variables.put("process.data.dir", "/opt/Bin2/process_data");
-
-            String binningHome = System.getenv(BINNING_HOME);
-            variables.put(BINNING_HOME, binningHome);
 
             ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("gs_diagnostic_binning", variables);
 
